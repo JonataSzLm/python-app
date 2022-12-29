9dd7a1d2104ffb37a871a3eb1b59f7a4a8eadcde
JonataSzLm <jonaatslima5@gmail.com>
2022-12-28 21:01:53 -0300

    sec

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+const showMenu = () => {
+    var navBar = document.querySelector('#nav')
+    navBar.classList.toggle('nav-hide')
+}
+
+const somar = () => {
+    let result = document.querySelector('.result')
+    let value1 = document.querySelector('.value1')
+    let value2 = document.querySelector('.value2')
+
+
+}
+const subtrair = () => {
+    let result = document.querySelector('.result')
+    let value1 = document.querySelector('.value1')
+    let value2 = document.querySelector('.value2')
+
+
+
+}
+const multiplicar = () => {
+    let result = document.querySelector('.result')
+    let value1 = document.querySelector('.value1')
+    let value2 = document.querySelector('.value2')
+
+
+
+}
+const dividir = () => {
+    let result = document.querySelector('.result')
+    let value1 = document.querySelector('.value1')
+    let value2 = document.querySelector('.value2')
+
+
+
+}
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import webview
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='./assets', template_folder='./templates')
 
 window = webview.create_window('Aplicação', app, min_size=(
-    800, 600), background_color='#0073ff', confirm_close=True)
+    800, 600), background_color='#000000', confirm_close=True)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', result='Resultado')
+
+@app.route('/somar', methods=['POST'])
+def somar():
+    value1 = request.form['value1']
+    value2 = request.form['value2']
+    result = value1 + value2
+    return render_template('index.html', result=result)
+
+
+@app.route('/subtrair', methods=['POST'])
+def subtrair():
+    value1 = request.form['value1']
+    value2 = request.form['value2']
+    result = value1 - value2
+    return render_template('index.html', result=result)
+
+
+@app.route('/multiplicar', methods=['POST'])
+def multiplicar():
+    value1 = request.form['value1']
+    value2 = request.form['value2']
+    result = value1 * value2
+    return render_template('index.html', result=result)
+
+
+@app.route('/dividir', methods=['POST'])
+def dividir():
+    value1 = request.form['value1']
+    value2 = request.form['value2']
+    result = value1 / value2
+    return render_template('index.html', result=result)
 
 
 if __name__ == '__main__':
